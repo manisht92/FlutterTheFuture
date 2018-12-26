5b3cd34abd8f9efb0dc0e7cf0e3728e58875ba82
Manish Tiwari <manish.t92@gmail.com>
2018-12-26 08:39:10 +0530

    Add Product with stateless widget(Initial Setup)

--- flutter_academind/lib/main.dart
+++ flutter_academind/lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("EasyList"),
         ),
+        body: Column(children: [
+          Container(
+              margin: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("Add Product"),
+                    onPressed: () {},
+                  )
+                ],
+              )),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/food.jpg"),
+                Text("Food Paradise")
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
